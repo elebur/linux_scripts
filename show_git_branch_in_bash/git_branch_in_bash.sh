bc08e2e848d78a3dfcb0d49b52ce246463f4151f
elebur <111658332+elebur@users.noreply.github.com>
2025-04-20 23:41:21 +0300

    make username bold

--- show_git_branch_in_bash/git_branch_in_bash.sh
+++ show_git_branch_in_bash/git_branch_in_bash.sh
@@ -1,6 +1,7 @@
 # Custom Bash Prompt
 # StackOverflow reference: https://stackoverflow.com/a/35218509/3684641
 # Customisation rules: https://help.ubuntu.com/community/CustomizingBashPrompt
+#   01 in \[\033[01;32m\] stands for bold font.
 # Original script: https://gist.github.com/rmharrison/1885ef6bbb0226eb7b42e2135d5eeca2#file-custombashprompt-sh
 
 # Installing:
@@ -8,4 +9,4 @@
 parse_git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\[\033[32m\]\u@\h:\[\033[34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]$ "
+export PS1="\[\033[01;32m\]\u@\h:\[\033[34m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]$ "
